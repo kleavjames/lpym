7ad10738ebb753718103866bde9034d6de0d3dce
Kleavant James <james@sparknspur.com>
2023-12-23 11:55:22 +0800

    revamp styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@emotion/react';
+import { CssVarsProvider } from '@mui/joy/styles';
 import theme from './theme.ts';
 import { HashRouter } from 'react-router-dom';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import '@fontsource/inter';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter basename='/'>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+      <CssVarsProvider theme={theme}>
         <App />
-      </ThemeProvider>
+      </CssVarsProvider>
     </HashRouter>
   </React.StrictMode>,
 )
